87cbeced9223780d9d9cca4280c0d6725faaff4c
Shahana700 <shahanacs2003@gmail.com>
2025-06-09 22:42:45 +0530

    update this file

--- basics/objects.js
+++ basics/objects.js
@@ -1,7 +1,8 @@
 //templates litrals
 let pencil = 10;
 let eraser = 5;
- let output = console.log(`the price of the pencil is ${pencil} + $ ${eraser} rupess`);
- let total = pencil + eraser;
- console.log(`the total price is ${total} rupess`);
+ let output = console.log(`the price of the pencil is ${pencil + eraser} rupess`);
+//  let total = pencil + eraser;
+//  console.log(`the total price is ${total} rupess`);
+
 
