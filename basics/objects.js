36e3515639149a85ff5dca3419030514a34962cb
Shahana700 <shahanacs2003@gmail.com>
2025-06-09 22:40:40 +0530

    adding a simple templates lirtals

---
+++ basics/objects.js
@@ -0,0 +1,7 @@
+//templates litrals
+let pencil = 10;
+let eraser = 5;
+ let output = console.log(`the price of the pencil is ${pencil} + $ ${eraser} rupess`);
+ let total = pencil + eraser;
+ console.log(`the total price is ${total} rupess`);
+
