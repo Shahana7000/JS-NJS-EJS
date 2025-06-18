6f750f260d2703001c5c029c5619451502762d7e
Shahana700 <shahanacs2003@gmail.com>
2025-06-18 19:19:42 +0530

    some updation

--- basics/concat.js
+++ basics/concat.js
@@ -7,3 +7,17 @@ console.log(third);
 let fruits = ["apple", "banana", "cherry"];
 let reversed = fruits.reverse();
 console.log(reversed);
+//tic-tic-toe game
+let game = [['x', null , 'o'],[null,'x',null],['o',null,'x']];
+game[0][1] = 'o';
+console.log(game);
+//WriteaJSprogramtodeletealloccurrencesofelement‘num’inagivenarray.Example:ifarr=[1,2,3,4,5,6,2,3]&num=2Resultshouldbearr=[1,3,4,5,6,3]
+let arr1 = [1,2,3,4,5,6,2,3];
+let num = 2;
+for(let i = 0;i<arr1.length;i++){
+    if(arr1[i] == num){
+        arr1.splice(i,1);
+    }
+}
+console.log(arr1);
+
