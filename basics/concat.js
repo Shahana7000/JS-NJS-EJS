4d22e6e2f9794a9929557883396d62ebe4d80215
Shahana700 <shahanacs2003@gmail.com>
2025-06-18 18:04:37 +0530

    somthing update

--- basics/concat.js
+++ basics/concat.js
@@ -1,4 +1,9 @@
+//concatination method
 let first = ["apple","banana", "cherry"];
 let second = ["banana", "cherry", "date"];
 let third = first.concat(second);
 console.log(third);
+//reverse method
+let fruits = ["apple", "banana", "cherry"];
+let reversed = fruits.reverse();
+console.log(reversed);
