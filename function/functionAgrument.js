e1b7dee9c6735070b3cdbbb9dfa96c524cd6e415
Shahana700 <shahanacs2003@gmail.com>
2025-06-27 23:49:36 +0530

    function where print tables of given n numbers

--- function/functionAgrument.js
+++ function/functionAgrument.js
@@ -1,4 +1,11 @@
 function printName(name){
     console.log(name);
 }
+function TablePrint(n){
+    for(let i = n;i<=n*10;i+=n){
+        console.log(i);
+    }
+
+}
+console.log(TablePrint(4));
 printName("shahana");
