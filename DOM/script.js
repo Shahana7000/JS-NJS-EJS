c031c273cf78a2e2f5de9123666922157503bb5c
Shahana700 <shahanacs2003@gmail.com>
2025-07-01 22:56:03 +0530

    some changes

--- DOM/script.js
+++ DOM/script.js
@@ -1,9 +1,9 @@
 let newBtn = document.createElement("button")
 newBtn.innerText = "click me!";
 
-newBtn.style.backgroundColor = "black";
+newBtn.style.backgroundColor = "blue";
 
-newBtn.style.color = "white";
+newBtn.style.color = "red";
 
 newBtn.style.height = "50px";
 newBtn.style.width = "100px";
