f7c3198616b5f79409070ca617bba187a2224899
Shahana700 <shahanacs2003@gmail.com>
2025-07-01 22:57:48 +0530

    add a new function

--- DOM/script.js
+++ DOM/script.js
@@ -1,9 +1,10 @@
 let newBtn = document.createElement("button")
 newBtn.innerText = "click me!";
 
-newBtn.style.backgroundColor = "blue";
+newBtn.style.backgroundColor = "yellow";
 
 newBtn.style.color = "red";
+newBtn.style.fontSize = "20px";
 
 newBtn.style.height = "50px";
 newBtn.style.width = "100px";
