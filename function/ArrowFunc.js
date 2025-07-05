781a1630b7964ae7f0de65746b3408c40e2f7f09
Shahana700 <shahanacs2003@gmail.com>
2025-07-05 13:34:24 +0530

    Arrow function

--- function/ArrowFunc.js
+++ function/ArrowFunc.js
@@ -2,8 +2,12 @@ const cube = (n) => {
     return n * n * n;
 };
 console.log(cube(5));
-// const area = (r) => {
-//     return 3.14 * r * r;
+const area = (r) => {
+    return 3.14 * r * r;
 
-// };
-// console.log(area(188) );// Output: 125
+};
+console.log(area(188) );// Output: 125
+const pow = (a , b) => {
+    return a ** b;
+};
+console.log(pow(2,3));
