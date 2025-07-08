b86dd3f84056ec36380cd7b33014086e8851ab5f
Shahana700 <shahanacs2003@gmail.com>
2025-07-08 15:18:41 +0530

    map function

--- function/ArrayMethod.js
+++ function/ArrayMethod.js
@@ -20,3 +20,12 @@ let str = [
 ];
 let single = str.map(user => user.name);
 console.log(single);
+
+
+//bill with GSt
+
+
+let bill = [200,340,50,333,1232];
+
+let withGSt = bill.map(bil => bil+10);
+console.log(withGSt);
