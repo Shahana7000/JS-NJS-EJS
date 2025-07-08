c8d37f10986be04e8bcfec5888a4990b414456a6
Shahana700 <shahanacs2003@gmail.com>
2025-07-08 15:15:39 +0530

    map function

--- function/ArrayMethod.js
+++ function/ArrayMethod.js
@@ -4,3 +4,19 @@ let double = numbers.map((el) =>{
     return el * el;
 });
 console.log(double);
+let str = [
+    {
+        name : "shahana",
+        age : 22,
+    },
+    {
+        name :"Saksham",
+        age : 20,
+    },
+    {
+        name :"Alisha",
+        age : 20
+    }
+];
+let single = str.map(user => user.age);
+console.log(single);
