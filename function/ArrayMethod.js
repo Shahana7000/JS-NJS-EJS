1131651ffdffed44fb3c4c4558206bd60b786ad2
Shahana700 <shahanacs2003@gmail.com>
2025-07-08 15:27:08 +0530

    filter methode adding

--- function/ArrayMethod.js
+++ function/ArrayMethod.js
@@ -29,3 +29,14 @@ let bill = [200,340,50,333,1232];
 
 let withGSt = bill.map(bil => bil+10);
 console.log(withGSt);
+
+// filter method 
+ let num = [1,2,3,4,5,6,7,8,9,10,11];
+
+ let even = num.filter((el) =>{
+    return el % 2==0
+ });
+ console.log(even);
+
+ 
+
